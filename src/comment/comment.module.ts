8fe7bc3183b012d76bce3d614c2ff1caad50d658
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-09-03 15:06:38 +0300

    Added comment service

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { Post } from 'src/post/post.model'
+import { TokenModule } from 'src/token/token.module'
 import { User } from 'src/user/user.model'
 import { CommentController } from './comment.controller'
 import { Comment } from './comment.model'
@@ -9,6 +10,9 @@ import { CommentService } from './comment.service'
 @Module({
   controllers: [CommentController],
   providers: [CommentService],
-  imports: [SequelizeModule.forFeature([Comment, User, Post])]
+  imports: [
+    SequelizeModule.forFeature([Comment, User, Post]),
+    TokenModule
+  ]
 })
 export class CommentModule {}
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/sequelize'
+import { I18nService } from 'nestjs-i18n'
+import { Comment } from './comment.model'
+import { CommentCreateDTO } from './DTO/comment-create.dto'
+import { CommentUpdateDTO } from './DTO/comment-update.dto'
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+    constructor(
+        @InjectModel(Comment) private readonly commentRepository: typeof Comment,
+        private readonly i18nService: I18nService
+    ) { }
+
+    async getByPostId(postId: string) {
+        const comments = await this.commentRepository.findAll({where: {postId: parseInt(postId)}})
+        return comments
+    }
+    
+    async create(dto: CommentCreateDTO & {userId: number}) {
+        const comment = await this.commentRepository.create({
+            userId: dto.userId,
+            postId: parseInt(dto.postId),
+            content: dto.content,
+        })
+        return comment
+    }
+
+    async update(dto: CommentUpdateDTO) {
+        await this.verify(parseInt(dto.commentId), true)
+        const comment = await this.commentRepository.findByPk(parseInt(dto.commentId))
+        comment.content = dto.content
+        await comment.save()
+        return comment
+    }
+
+    async delete(commentId: string) {
+        return this.commentRepository.destroy({where: {id: parseInt(commentId)}})
+    }
+
+    async verify(commentId: number, mustHave = false) {
+        const hasPost = !!await this.commentRepository.findByPk(commentId)
+        if (hasPost !== mustHave) {
+            throw new HttpException(this.i18nService.t<string>('exception.comment.verify.not-found'), HttpStatus.NOT_FOUND)
+        }
+    }
+}
