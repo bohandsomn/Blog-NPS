dc20ded6fb7a7850825e6b433ca14867d97045cf
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-09-29 21:02:58 +0300

    Solved the problem with displaying the Authorization API response

--- src/authorization/DTO/authorization-user-data-response.dto.ts
+++ src/authorization/DTO/authorization-user-data-response.dto.ts
@@ -1,6 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { UserResponseDTO } from 'src/user/DTO/user-response.dto'
 
 export class AuthorizationUserDataResponseDTO {
+    @ApiProperty({ example: 'token' })
     readonly accessToken: string
+
+    @ApiProperty({type: UserResponseDTO})
     readonly user: UserResponseDTO
 }
