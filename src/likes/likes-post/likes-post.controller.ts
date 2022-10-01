5a9877119c2213c8f3473fc027f97f844dce1e02
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-01 13:58:46 +0300

    Solved the problem with displaying the Like API response

--- src/likes/likes-post/likes-post.controller.ts
+++ src/likes/likes-post/likes-post.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Param, Post, Req, UseGuards, UseInterceptors } from '@nestjs/common'
+import { Controller, Param, Post, Req, UseGuards, UseInterceptors, HttpStatus } from '@nestjs/common'
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { AuthorizationGuard, RequestUser } from 'src/authorization/authorization.guard'
 import { TransformServerMessageInterceptor } from 'src/transform/transform-server-message.interceptor'
@@ -14,7 +14,7 @@ export class LikesPostController {
     ) { }
 
     @ApiOperation({summary: 'Set like'})
-    @ApiResponse({status: 200, type: LikesPost})
+    @ApiResponse({status: HttpStatus.OK, type: LikesPost})
     @Post('like')
     @UseGuards(AuthorizationGuard)
     like(@Req() request: RequestUser, @Param('postId') postId: string) {
@@ -22,7 +22,7 @@ export class LikesPostController {
     }
 
     @ApiOperation({summary: 'Set dislike'})
-    @ApiResponse({status: 200, type: LikesPost})
+    @ApiResponse({status: HttpStatus.OK, type: LikesPost})
     @Post('dislike')
     @UseGuards(AuthorizationGuard)
     dislike(@Req() request: RequestUser, @Param('postId') postId: string) {
@@ -30,7 +30,7 @@ export class LikesPostController {
     }
 
     @ApiOperation({summary: 'Unlike'})
-    @ApiResponse({status: 200, type: LikesPost})
+    @ApiResponse({status: HttpStatus.OK, type: LikesPost})
     @Post('unlike')
     @UseGuards(AuthorizationGuard)
     unlike(@Req() request: RequestUser, @Param('postId') postId: string) {
