3adfea62cbbd6584fd8517edcc77f4a54af531da
bohandsomn <bohdan.lukianchenko@gmail.com>
2022-10-01 14:39:04 +0300

    Solved the problem with displaying the User API response

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Param, Put, Req, Res, UseGuards, UseInterceptors, UsePipes } from '@nestjs/common'
+import { Body, Controller, Get, Param, Put, Req, Res, UseGuards, UseInterceptors, UsePipes, HttpStatus } from '@nestjs/common'
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { Response } from 'express'
 import { AuthorizationGuard, RequestUser } from 'src/authorization/authorization.guard'
 import { AuthorizationUserDataResponseDTO } from 'src/authorization/DTO/authorization-user-data-response.dto'
+import { DocumentationHttpExceptionDTO } from 'src/documentation/documentation.http-exception.dto'
 import { TransformServerMessageInterceptor } from 'src/transform/transform-server-message.interceptor'
 import { ValidationPipe } from 'src/validation/validation.pipe'
 
@@ -19,6 +20,9 @@ export class UserController {
     ) { }
 
     @ApiOperation({summary: 'User update'})
+    @ApiResponse({status: HttpStatus.OK, type: AuthorizationUserDataResponseDTO})
+    @ApiResponse({status: HttpStatus.NOT_FOUND, type: DocumentationHttpExceptionDTO})
+    @ApiResponse({status: HttpStatus.CONFLICT, type: DocumentationHttpExceptionDTO})
     @Put()
     @UseGuards(AuthorizationGuard)
     @UsePipes(ValidationPipe)
@@ -32,7 +36,9 @@ export class UserController {
     }
 
     @ApiOperation({summary: 'User receive'})
-    @ApiResponse({status: 200, type: UserResponseDTO})
+    @ApiResponse({status: HttpStatus.OK, type: UserResponseDTO})
+    @ApiResponse({status: HttpStatus.CONFLICT, type: DocumentationHttpExceptionDTO})
+    @ApiResponse({status: HttpStatus.NOT_FOUND, type: DocumentationHttpExceptionDTO})
     @Get('/:userId')
     getOne(@Param('userId') userId: string) {
         return this.userService.getOne(parseInt(userId))
